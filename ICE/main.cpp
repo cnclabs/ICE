3e3cbf5cb9ba71d9182d9277042a9606e06b6940
Chih-Ming Chen <changecandy@gmail.com>
2017-09-19 16:01:02 +0800

    use abbreviated params

--- ICE/main.cpp
+++ ICE/main.cpp
@@ -25,18 +25,18 @@ int main(int argc, char **argv){
         printf("\t\tTrain the Network data\n");
         printf("\t-save <string>\n");
         printf("\t\tSave the representation data\n");
-        printf("\t-dimensions <int>\n");
+        printf("\t-dim <int>\n");
         printf("\t\tDimension of vertex representation; default is 64\n");
-        printf("\t-negative_samples <int>\n");
+        printf("\t-neg <int>\n");
         printf("\t\tNumber of negative examples; default is 5\n");
-        printf("\t-sample_times <int>\n");
+        printf("\t-sample <int>\n");
         printf("\t\tNumber of training samples *Million; default is 10\n");
-        printf("\t-threads <int>\n");
+        printf("\t-thread <int>\n");
         printf("\t\tNumber of training threads; default is 1\n");
         printf("\t-alpha <float>\n");
         printf("\t\tInit learning rate; default is 0.025\n");
         printf("\nExample Usage:\n");
-        printf("\t./cli -train network.txt -save rep.txt -dimensions 64 -sample_times 10 -negative_samples 5 -alpha 0.025 -threads 4\n\n");
+        printf("\t./cli -train network.txt -save rep.txt -dim 64 -sample 10 -neg 5 -alpha 0.025 -thread 4\n\n");
         return 0;
     }
     
@@ -46,11 +46,11 @@ int main(int argc, char **argv){
 
     if ((i = ArgPos((char *)"-train", argc, argv)) > 0) strcpy(network_file, argv[i + 1]);
     if ((i = ArgPos((char *)"-save", argc, argv)) > 0) strcpy(rep_file, argv[i + 1]);
-    if ((i = ArgPos((char *)"-dimensions", argc, argv)) > 0) dimensions = atoi(argv[i + 1]);
-    if ((i = ArgPos((char *)"-sample_times", argc, argv)) > 0) sample_times = atoi(argv[i + 1]);
-    if ((i = ArgPos((char *)"-negative_samples", argc, argv)) > 0) negative_samples = atoi(argv[i + 1]);
+    if ((i = ArgPos((char *)"-dim", argc, argv)) > 0) dimensions = atoi(argv[i + 1]);
+    if ((i = ArgPos((char *)"-sample", argc, argv)) > 0) sample_times = atoi(argv[i + 1]);
+    if ((i = ArgPos((char *)"-neg", argc, argv)) > 0) negative_samples = atoi(argv[i + 1]);
     if ((i = ArgPos((char *)"-alpha", argc, argv)) > 0) init_alpha = atof(argv[i + 1]);
-    if ((i = ArgPos((char *)"-threads", argc, argv)) > 0) threads = atoi(argv[i + 1]);
+    if ((i = ArgPos((char *)"-thread", argc, argv)) > 0) threads = atoi(argv[i + 1]);
 
     ICE *ice;
     ice = new ICE();

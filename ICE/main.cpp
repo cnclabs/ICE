0cb6c04998618245c93f268f706b2ff2bced51b8
LonSilent <101703044@nccu.edu.tw>
2017-11-26 00:21:19 +0800

    load weights at any stage

--- ICE/main.cpp
+++ ICE/main.cpp
@@ -50,7 +50,8 @@ int main(int argc, char **argv){
         return 0;
     }
     
-    char text_file[100], text_rep_file[100], text_context_file[100], entity_file[100], result_file[100];
+    char text_file[100], text_rep_file[100], text_context_file[100], entity_file[100] = {'\0'}, result_file[100];
+    char load_stage1_file[100] = {'\0'}, load_stage2_file[100] = {'\0'};
     int dimensions=64, negative_samples=5, sample_times=10, save_times=1, threads=1;
     double init_alpha=0.025;
 
@@ -60,6 +61,8 @@ int main(int argc, char **argv){
     if ((i = ArgPos((char *)"-entity", argc, argv)) > 0) strcpy(entity_file, argv[i + 1]);
     if ((i = ArgPos((char *)"-textrep", argc, argv)) > 0) strcpy(text_rep_file, argv[i + 1]);
     if ((i = ArgPos((char *)"-textcontext", argc, argv)) > 0) strcpy(text_context_file, argv[i + 1]);
+    if ((i = ArgPos((char *)"-load_embd1", argc, argv)) > 0) strcpy(load_stage1_file, argv[i + 1]);
+    if ((i = ArgPos((char *)"-load_embd2", argc, argv)) > 0) strcpy(load_stage2_file, argv[i + 1]);
     if ((i = ArgPos((char *)"-save", argc, argv)) > 0) strcpy(result_file, argv[i + 1]);
     if ((i = ArgPos((char *)"-save_times", argc, argv)) > 0) save_times = atoi(argv[i + 1]);
     if ((i = ArgPos((char *)"-dim", argc, argv)) > 0) dimensions = atoi(argv[i + 1]);
@@ -72,6 +75,8 @@ int main(int argc, char **argv){
     ice = new ICE();
     ice->LoadEdgeList(text_file);
     ice->Init(dimensions);
+    if (strlen(load_stage1_file))
+        ice->LoadWeights(load_stage1_file);
     ice->getVocabCount();
     
     int sub_sample_times = sample_times/save_times, current_sample_times=0;
@@ -98,10 +103,16 @@ int main(int argc, char **argv){
 
         }
     }
-
+    if (strlen(entity_file) == 0)
+        return 0;
     ice->LoadEdgeList(entity_file);
     ice->Init(dimensions);
-    ice->LoadWeights(text_context_file);
+    if (strlen(load_stage2_file)){
+        ice->LoadWeights(load_stage2_file);
+    }
+    else {
+        ice->LoadWeights(text_context_file);
+    }
 
     sub_sample_times = sample_times/save_times, current_sample_times=0;
     alpha_max=init_alpha, alpha_min=init_alpha;
